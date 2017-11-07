51680760f0f7965036be79cd5aafc78492deafde
naga_pulivarthy <naga_pulivarthy@epam.com>
2017-11-07 19:15:40 +0530

    Base version of MVC Helper Methods

---
+++ HelperMethods/HelperMethods/Controllers/HomeController.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace HelperMethods.Controllers
+{
+    public class HomeController : Controller
+    {
+        // GET: Home
+        public ActionResult Index()
+        {
+            ViewBag.Fruits = new String[] { "Grapes", "Apple", "Orange"};
+            ViewBag.States = new String[] { "TS", "Simla", "AP" };
+
+            return View();
+        }
+
+        public ActionResult Step1()
+        {
+            ViewBag.Fruits = new String[] { "Grapes", "Apple", "Orange" };
+            ViewBag.States = new String[] { "TS", "Simla", "AP" };
+
+            return View();
+        }
+    }
+}
